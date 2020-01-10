9f9af499ea7474100ed1859c6119aac569cdcef3
Jens Klingenberg <mail@jensklingenberg.de>
2020-01-10 17:05:46 +0100

    its working

---
+++ StackAttackLibrary/build/generated/source/buildConfig/debug/com/wagoodman/stackattack/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.wagoodman.stackattack;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.wagoodman.stackattack";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.wagoodman.stackattack";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "library";
+}
